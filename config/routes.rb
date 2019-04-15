efb07362da7e49720656d5c6da6d74eefc970f1e
joe <kinjoyuji@gmail.com>
2019-04-15 18:32:32 +1000

    hello React

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  # frontend
+  root 'welcomes#index'
+
+  # backend
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
