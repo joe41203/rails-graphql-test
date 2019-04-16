974aa369bd90d66921ce2d51a286459c54bcbc2b
joe <kinjoyuji@gmail.com>
2019-04-16 12:44:06 +1000

    setting factory generator

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module RailsGraphqlTest
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.factory_bot dir: 'spec/factories'
+    end
   end
 end
